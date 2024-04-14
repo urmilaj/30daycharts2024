5413c151f661ed8ffb2b3d6a57a3d06b0d1f8966
urmilaj <urmila.j@hotmail.com>
2024-04-14 19:36:20 +0530

    fix error

--- observablehq.config.ts
+++ observablehq.config.ts
@@ -32,34 +32,34 @@ export default {
     {
       name: "Week 3 - Relationships",
       pages: [
-        {name: "Day 14 - Family", path: "/family"},
-        // {name: "Day 15 - Heatmap", path: ""},
-        // {name: "Day 16 - Historical", path: ""},
-        // {name: "Day 17 - Weather", path: ""},
-        // {name: "Day 18 - Networks", path: ""},
-        // {name: "Day 19 - Asian Development Bank (data day)", path: ""},
+        {name: "Day 13 - Family", path: "/family"},
+        // {name: "Day 14 - Heatmap", path: ""},
+        // {name: "Day 15 - Historical", path: ""},
+        // {name: "Day 16 - Weather", path: ""},
+        // {name: "Day 17 - Networks", path: ""},
+        // {name: "Day 18 - Asian Development Bank (data day)", path: ""},
       ]
     },
     // {
     //   name: "Week 4 - Timeseries",
     //   pages: [
-    //     {name: "Day 20 - Dinosaurs", path: ""},
-    //     {name: "Day 21 - Correlation", path: ""},
-    //     {name: "Day 22 - Green Energy", path: ""},
-    //     {name: "Day 23 - Mobility", path: ""},
-    //     {name: "Day 24 - Tiles", path: ""},
-    //     {name: "Day 25 - ILO Region for Africa (data day)", path: ""},
+    //     {name: "Day 19 - Dinosaurs", path: ""},
+    //     {name: "Day 20 - Correlation", path: ""},
+    //     {name: "Day 21 - Green Energy", path: ""},
+    //     {name: "Day 22 - Mobility", path: ""},
+    //     {name: "Day 23 - Tiles", path: ""},
+    //     {name: "Day 24 - ILO Region for Africa (data day)", path: ""},
     //   ]
     // },
     // {
     //   name: "Week 5 - Uncertainties",
     //   pages: [
-    //     {name: "Day 26 - Global Change", path: ""},
-    //     {name: "Day 27 - AI", path: ""},
-    //     {name: "Day 28 - Good/Bad", path: ""},
-    //     {name: "Day 29 - Trend", path: ""},
-    //     {name: "Day 30 - Black'n'White", path: ""},
-    //     {name: "Day 31 - FiveThirtyEight (theme day)", path: ""},
+    //     {name: "Day 25 - Global Change", path: ""},
+    //     {name: "Day 26 - AI", path: ""},
+    //     {name: "Day 27 - Good/Bad", path: ""},
+    //     {name: "Day 28 - Trend", path: ""},
+    //     {name: "Day 29 - Black'n'White", path: ""},
+    //     {name: "Day 30 - FiveThirtyEight (theme day)", path: ""},
     //   ]
     // }
   ],
